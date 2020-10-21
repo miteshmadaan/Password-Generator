faf7616f12e1df3d2897439d28dc09666c9f0d46
miteshmadaan <48414028+miteshmadaan@users.noreply.github.com>
2020-10-21 12:33:19 +0530

    #Supporting Modules

--- sendMAIL_github.py
+++ sendMAIL_github.py
@@ -2,7 +2,7 @@
 import smtplib
 def send_mail(password,email_id):#getting input for password and receiver's email id
     s = smtplib.SMTP('smtp.gmail.com', 587)#setting of server id
-    s.starttls()#generating of secure login pathway
+    s.starttls()#generating of secure login pathway 
     s.login(<sender's email ID>, <sender's Email ID password>)#login details
     message = "\n\nHello Buddy. I am <sender's name>.\nAs a part of my project Password Generator which uses letters and digits from" \
                    " your name, id no. and date of birth and generates a strongest possible password.\n" \
